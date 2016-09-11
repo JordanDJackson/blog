b09756d001ef65f78c6bb502c6b93fe60e2ffcf0
Jordan D. Jackson <jordandjackson21@gmail.com>
2016-09-11 04:43:50 -0700

    New article button

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -6,6 +6,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   def new
+    @article = Article.new
 
+  end
+  def create
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.save
+    redirect_to article_path(@article)
   end
 end
