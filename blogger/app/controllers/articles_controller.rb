096ae0f847cf1d20bb16424ea3dc152836928133
Jordan D. Jackson <jordandjackson21@gmail.com>
2016-09-14 01:08:08 -0700

    Delete and edit

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -10,9 +11,24 @@ class ArticlesController < ApplicationController
 
   end
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
+    @article = Article.new(article_params)
     @article.save
+    redirect_to article_path(@article)
+  end
+  def destroy
+    @art = Article.find(params[:id])
+    @art.destroy
+    redirect_to article_path(articles)
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!'}""
+
     redirect_to article_path(@article)
   end
 end
